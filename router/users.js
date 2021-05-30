b1dd44ac011c9126a3b74f5068d07be79588024c
hadipsy27 <hadipsy27@gmail.com>
2021-05-30 22:34:33 +0700

    response tiap router users

--- router/users.js
+++ router/users.js
@@ -8,14 +8,29 @@ let users = [
 
 router.route('/users')
   .get(function(req, res){
-    // res.send('Get Users')
-    res.json(users)
+    if(users.length > 0){
+      res.json({
+        status: true,
+        data: users,
+        method: req.method,
+        url: req.url
+      })
+    } else{
+      res.json({
+        status: false,
+        message: "Data Users masih kosong sob!"
+      })
+    }
   })
   .post(function(req, res){
-    // res.send('Post Users')
     users.push(req.body)
-    res.json(users)
-
+    res.json({
+      status: true,
+      data: users,
+      message: "Data User berhasil disimpan sob!",
+      method: req.method,
+      url: req.url
+    })
   })
 
 router.put('/users/:id', function(req, res){
@@ -29,14 +44,26 @@ router.put('/users/:id', function(req, res){
       return user
     }
   })
-  res.json(users)
+  res.json({
+    status: true,
+    data: users,
+    message: "Data User berhasil diedit sob!",
+    method: req.method,
+    url: req.url
+  })
 })
 
 router.delete('/users/:userId', function(req, res){
   // res.send(req.params.userId)
   let id = req.params.userId
-  users = users.filter(user => user.id != id) // jika id user sama maka di hapus(di filter), jika beda maka di biarkan 
-  res.send(users)
+  users = users.filter(user => user.id != id) // jika id user sama maka di hapus(di filter), jika beda maka di biarkan
+  res.send({
+    status: true,
+    data: users,
+    message: "Data User berhasil dihapus sob!",
+    method: req.method,
+    url: req.url
+  })
 })
 
 module.exports = router
